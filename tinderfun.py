bbc9cb5d606b10ffebc6fb6d5cac84f1402167f1
thomas.friedel <thomas.friedel@berlin>
2016-06-02 21:10:20 +0200

    tinderfun.py swipes everyone right Tinder.ipynb is a notebook that shows the last 40 matches

--- tinderfun.py
+++ tinderfun.py
@@ -1,7 +1,9 @@
 import pynder
 from datetime import timedelta, datetime
+import random
+from pickle import dump, load
 from private import *
-session = pynder.Session(facebook_id=facebook_id, facebook_token=facebook_token)
+import time
 
 
 def get_ping_time(user):
@@ -15,3 +17,25 @@ def get_active_users(matches):
 def get_user_named(matches, name):
     filtered_matches = [m for m in matches if str(m) == "u'{}'".format(name)]
     return filtered_matches[0]
+
+def like_all(session, ids):
+    counter = 0
+    while session.likes_remaining > 0:
+        users = session.nearby_users()
+        for user in users:
+            time.sleep(random.random() / 5.0)
+            if not user.id in ids:
+                like = user.like()
+            else:
+                like = user.dislike()
+                print("user already matched:")
+            counter += 1
+            print(user.name)
+    print ("liked {}".format(counter))
+
+# this script swipes everyone right
+session = pynder.Session(facebook_id=facebook_id, facebook_token=facebook_token)
+matches = session.matches()
+
+ids = [match.user.id for match in matches if match.user]
+like_all(session, ids)
